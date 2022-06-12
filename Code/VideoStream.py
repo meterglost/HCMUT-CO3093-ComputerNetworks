bd71f3c915d94691e1e3762024cbb382093232a5
meterglost <54196381+meterglost@users.noreply.github.com>
2022-06-13 02:28:52 +0700

    Initial code
    Only format the code and correct grammar mistake

--- Code/VideoStream.py
+++ Code/VideoStream.py
@@ -6,20 +6,19 @@ class VideoStream:
 		except:
 			raise IOError
 		self.frameNum = 0
-		
+
 	def nextFrame(self):
 		"""Get next frame."""
-		data = self.file.read(5) # Get the framelength from the first 5 bits
-		if data: 
+		# Get the framelength from the first 5 bytes
+		data = self.file.read(5)
+		if data:
 			framelength = int(data)
-							
+
 			# Read the current frame
 			data = self.file.read(framelength)
 			self.frameNum += 1
 		return data
-		
+
 	def frameNbr(self):
 		"""Get frame number."""
 		return self.frameNum
-	
-	
